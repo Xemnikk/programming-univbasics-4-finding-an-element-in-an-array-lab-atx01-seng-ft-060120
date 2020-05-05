078ef8a2fe6766cb071c118114e3cd72f7234817
Toby Boudreaux <toby.boudreaux11@gmail.com>
2020-05-05 15:04:14 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -4,6 +4,9 @@ def find_element_index(array, value_to_find)
   index = nil 
   while counter < array.length do
     if array[counter] == value_to_find
+      index = counter
+    end 
     counter += 1 
   end 
+  index 
 end
